76f4a17cb2334b0f5a5ee1cce2a7a903dc0434bb
amarettnny <ssun55@jhu.edu>
2025-02-05 20:56:10 -0500

    passed all basic unit tests for all functions

--- HW1/README.txt
+++ HW1/README.txt
@@ -4,3 +4,13 @@ For Milestone 1, Siyang Sun wrote the following 2 functions:
 and Tianji Li wrote the following 2 functions: 
     uint256_get_bits
     uint256_is_bit_set
+
+For Milestone 2, Tianji wrote the following functions with their unit tests:
+    uint256_create_from_hex
+    uint256_format_as_hex
+    uint256_add
+    uint256_sub
+and Siyang Sun wrote the following functions with their unit tests:
+    uint256_negate
+    uint256_mul
+    uint256_lshift
--- HW1/uint256.c
+++ HW1/uint256.c
@@ -1,18 +1,19 @@
-#include <assert.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "uint256.h"
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Create a UInt256 value from a single uint32_t value.
 // Only the least-significant 32 bits are initialized directly,
 // all other bits are set to 0.
-UInt256 uint256_create_from_u32( uint32_t val ) {
+UInt256 uint256_create_from_u32(uint32_t val) {
   UInt256 result;
-  result.data[0] = val; //set the leftmost(LSB) to val
+  result.data[0] = val; // set the leftmost to val
 
-  //set the rest to zeros
-  for (int i = 1; i < 8; i++){
+  // set the rest to zeros
+  for (int i = 1; i < 8; i++) {
     result.data[i] = 0;
   }
   return result;
@@ -21,9 +22,9 @@ UInt256 uint256_create_from_u32( uint32_t val ) {
 // Create a UInt256 value from an array of NWORDS uint32_t values.
 // The element at index 0 is the least significant, and the element
 // at index 7 is the most significant.
-UInt256 uint256_create( const uint32_t data[8] ) {
+UInt256 uint256_create(const uint32_t data[8]) {
   UInt256 result;
-  
+
   for (int i = 0; i < 8; i++) {
     result.data[i] = data[i];
   }
@@ -31,33 +32,32 @@ UInt256 uint256_create( const uint32_t data[8] ) {
 }
 
 // Create a UInt256 value from a string of hexadecimal digits.
-UInt256 uint256_create_from_hex( const char *hex ) {
+UInt256 uint256_create_from_hex(const char *hex) {
   UInt256 result;
-  // TODO: implement
   int len = strlen(hex);
-  const char* start;
+  const char *start;
   int count;
   int remain;
   // if length is greater than 64, use the rightmost 64
-  if (len > 64){
+  if (len > 64) {
     start = hex + len - 64;
     count = 8;
     remain = 0;
-  }else{
+  } else {
     start = hex;
     count = len / 8;
     remain = len % 8;
   }
   // start from the rightmost 8 char, initailize uint256 value
   int idx = 0;
-  while (idx < count){
+  while (idx < count) {
     char hex_str[9];
     strncpy(hex_str, start + remain + (count - idx - 1) * 8, 8);
     hex_str[8] = '\0';
     result.data[idx] = strtoul(hex_str, NULL, 16);
     idx += 1;
   }
-  if (remain != 0 && idx < 8){
+  if (remain != 0 && idx < 8) {
     char hex_str[remain + 1];
     strncpy(hex_str, start, remain);
     hex_str[remain] = '\0';
@@ -65,7 +65,7 @@ UInt256 uint256_create_from_hex( const char *hex ) {
     idx += 1;
   }
   // fill 0 for the uninitailized part
-  while(idx < 8){
+  while (idx < 8) {
     result.data[idx] = 0;
     idx += 1;
   }
@@ -74,30 +74,29 @@ UInt256 uint256_create_from_hex( const char *hex ) {
 
 // Return a dynamically-allocated string of hex digits representing the
 // given UInt256 value.
-char *uint256_format_as_hex( UInt256 val ) {
+char *uint256_format_as_hex(UInt256 val) {
   char *hex = NULL;
-  // TODO: implement
-  //find the part of uint256 to start
+  // find the part of uint256 to start
   int idx = 7;
-  while (idx >= 0 && val.data[idx] == 0){
+  while (idx >= 0 && val.data[idx] == 0) {
     idx--;
   }
-  //if uint256 value is 0, output "0"
-  if (idx == -1){
+  // if uint256 value is 0, output "0"
+  if (idx == -1) {
     hex = malloc(2);
     strcpy(hex, "0");
     return hex;
   }
   // get the hex string for each part
-  char* strings[idx + 1];
+  char *strings[idx + 1];
   int sum_len = 0;
-  for(int i = 0; i <= idx; i++){
+  for (int i = 0; i <= idx; i++) {
     strings[i] = malloc(9);
     uint32_t value = val.data[idx - i];
-    if (i == 0){
+    if (i == 0) {
       sprintf(strings[i], "%x", value);
       sum_len += strlen(strings[i]);
-    }else{
+    } else {
       sprintf(strings[i], "%08x", value);
       sum_len += 8;
     }
@@ -105,10 +104,10 @@ char *uint256_format_as_hex( UInt256 val ) {
   // combine the hex strings together as a output
   hex = malloc(sum_len + 1);
   strcpy(hex, strings[0]);
-  for(int i = 1; i <= idx; i++){
+  for (int i = 1; i <= idx; i++) {
     strcat(hex, strings[i]);
   }
-  for(int i = 0; i <= idx; i++){
+  for (int i = 0; i <= idx; i++) {
     free(strings[i]);
   }
   return hex;
@@ -117,16 +116,14 @@ char *uint256_format_as_hex( UInt256 val ) {
 // Get 32 bits of data from a UInt256 value.
 // Index 0 is the least significant 32 bits, index 7 is the most
 // significant 32 bits.
-uint32_t uint256_get_bits( UInt256 val, unsigned index ) {
+uint32_t uint256_get_bits(UInt256 val, unsigned index) {
   uint32_t bits;
-  // TODO: implement
   bits = val.data[index];
   return bits;
 }
 
 // Return 1 if bit at given index is set, 0 otherwise.
-int uint256_is_bit_set( UInt256 val, unsigned index ) {
-  // TODO: implement
+int uint256_is_bit_set(UInt256 val, unsigned index) {
   int bit_idx = index / 32;
   int idx = index % 32;
   int bit = (int)((val.data[bit_idx] << (31 - idx)) >> 31);
@@ -134,19 +131,19 @@ int uint256_is_bit_set( UInt256 val, unsigned index ) {
 }
 
 // Compute the sum of two UInt256 values.
-UInt256 uint256_add( UInt256 left, UInt256 right ) {
+UInt256 uint256_add(UInt256 left, UInt256 right) {
   UInt256 sum;
-  // TODO: implement
   int overflow = 0;
-  for(int i = 0; i < 8; i++){
+  for (int i = 0; i < 8; i++) {
     uint32_t leftpart = left.data[i];
     uint32_t rightpart = right.data[i];
     uint32_t total = leftpart + rightpart + overflow;
     sum.data[i] = total;
     // check whether there is an overflow
-    if ((overflow == 0 && total < leftpart) || (overflow == 1 && total <= leftpart)){
+    if ((overflow == 0 && total < leftpart) ||
+        (overflow == 1 && total <= leftpart)) {
       overflow = 1;
-    }else{
+    } else {
       overflow = 0;
     }
   }
@@ -154,7 +151,7 @@ UInt256 uint256_add( UInt256 left, UInt256 right ) {
 }
 
 // Compute the difference of two UInt256 values.
-UInt256 uint256_sub( UInt256 left, UInt256 right ) {
+UInt256 uint256_sub(UInt256 left, UInt256 right) {
   UInt256 result;
   // TODO: implement
   UInt256 nright = uint256_negate(right);
@@ -163,22 +160,61 @@ UInt256 uint256_sub( UInt256 left, UInt256 right ) {
 }
 
 // Return the two's-complement negation of the given UInt256 value.
-UInt256 uint256_negate( UInt256 val ) {
+UInt256 uint256_negate(UInt256 val) {
   UInt256 result;
-  // TODO: implement
+
+  // Inverting all bits
+  for (int i = 0; i < 8; i++) {
+    result.data[i] = ~val.data[i]; // negating all bits for each uint32
+  }
+
+  // Add 1 for 2's complement's negation
+  UInt256 one = uint256_create_from_u32(1);
+  result = uint256_add(result, one);
   return result;
 }
 
 // Compute the product of two UInt256 values.
-UInt256 uint256_mul( UInt256 left, UInt256 right ) {
-  UInt256 product;
-  // TODO: implement
+UInt256 uint256_mul(UInt256 left, UInt256 right) {
+  // Initialize the product with 0
+  UInt256 product = {0};
+  
+  // Iterate through all bits that has been set
+  for (int i = 0; i < 256; i ++){
+    if (uint256_is_bit_set(left, i)){
+      // left shifting 'right' by the bit position of the bit in 'left'
+      UInt256 temp = uint256_lshift(right, i);
+      product = uint256_add(product, temp);
+    }
+  }
+
   return product;
 }
 
-UInt256 uint256_lshift( UInt256 val, unsigned shift ) {
-  assert( shift < 256 );
-  UInt256 result;
-  // TODO: implement
+UInt256 uint256_lshift(UInt256 val, unsigned shift) {
+  assert(shift < 256); // undefined behavior
+
+  // Initializing the result with 0
+  UInt256 result = {0};
+
+  int element_shift = shift / 32; // 32-bits element need to shift
+  int bits_shift = shift % 32;    // individual bits need to shift
+
+  if (bits_shift > 0) {
+    for (int i = 7; i >= element_shift; i--) {
+      // shifting elements and bits
+      result.data[i] = val.data[i - element_shift] << bits_shift;
+
+      // handle overflow from the previous element
+      if ((i - element_shift) >= 0) {
+        uint32_t overflow = val.data[i - element_shift - 1] >> (32 - bits_shift);
+        result.data[i] = result.data[i] | overflow; //bitwise OR
+      }
+    }
+  } else { // only shift elements (shift is divisible by 32)
+    for (int i = 7; i >= element_shift; i--) {
+      result.data[i] = val.data[i - element_shift];
+    }
+  }
   return result;
 }
