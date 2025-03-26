d23ca8c23731f67f596d309db0291a43c7b2b75a
amarettnny <ssun55@jhu.edu>
2025-03-26 12:07:07 -0400

    starter code for MS2

---
+++ csf_assign03/cache_sim.cpp
@@ -0,0 +1,6 @@
+#include "cache_sim.h"
+
+//helper functions in this file --> need to add them in header file as well
+// 1. valid_input()
+// 2. Cache struct initializing
+// 3. Cache load/read handle
