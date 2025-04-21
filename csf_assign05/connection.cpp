715d21972d6ee81c3305eeef635aa62e0e732ec5
Tianji Li <tianjili66@gmail.com>
2025-04-20 20:37:18 -0400

    fix error in receiver

--- csf_assign05/connection.cpp
+++ csf_assign05/connection.cpp
@@ -13,12 +13,12 @@ Connection::Connection()
 Connection::Connection(int fd)
   : m_fd(fd)
   , m_last_result(SUCCESS) {
-  // TODO: call rio_readinitb to initialize the rio_t object
+  // call rio_readinitb to initialize the rio_t object
   rio_readinitb(&m_fdbuf, fd);
 }
 
 void Connection::connect(const std::string &hostname, int port) {
-  // TODO: call open_clientfd to connect to the server
+  //call open_clientfd to connect to the server
   std::string port_str = std::to_string(port);  
   int socket_descriptor = open_clientfd(hostname.c_str(), port_str.c_str());
   if (socket_descriptor == -1 || socket_descriptor == -2){
@@ -26,14 +26,14 @@ void Connection::connect(const std::string &hostname, int port) {
     m_fd = -1;
     return;
   }
-  // TODO: call rio_readinitb to initialize the rio_t object
+  //call rio_readinitb to initialize the rio_t object
   m_fd = socket_descriptor;
   m_last_result = SUCCESS;
   rio_readinitb(&m_fdbuf, m_fd);
 }
 
 Connection::~Connection() {
-  // TODO: close the socket if it is open
+  //close the socket if it is open
   if (is_open()){
     Close(m_fd);
     m_fd = -1;
@@ -41,7 +41,7 @@ Connection::~Connection() {
 }
 
 bool Connection::is_open() const {
-  // TODO: return true if the connection is open
+  //return true if the connection is open
   if (m_fd >= 0){
     return true;
   }
@@ -49,7 +49,7 @@ bool Connection::is_open() const {
 }
 
 void Connection::close() {
-  // TODO: close the connection if it is open
+  //close the connection if it is open
   if (is_open()){
     Close(m_fd);
     m_fd = -1;
@@ -57,16 +57,24 @@ void Connection::close() {
 }
 
 bool Connection::send(const Message &msg) {
-  // TODO: send a message
-  // return true if successful, false if not
-  // make sure that m_last_result is set appropriately
+  // send a message return true if successful, false if not
+  // set m_last_result appropriately
   std::string data = msg.data;
-  std::string tag = msg.tag;  
+  std::string tag = msg.tag;
+  // check encoded message is within the required length  
   if ((data.length() + 2 + tag.length()) > Message::MAX_LEN){
     m_last_result = INVALID_MSG;
     return false;
   }
-  // do I need to check tag is valid?
+  // check tag is valid
+  if(tag != TAG_ERR && tag != TAG_OK && tag != TAG_SLOGIN && tag != TAG_RLOGIN && 
+		  tag != TAG_JOIN && tag != TAG_LEAVE && tag != TAG_SENDALL && 
+		  tag != TAG_SENDUSER && tag != TAG_QUIT && tag != TAG_DELIVERY
+		  && tag != TAG_EMPTY){
+    m_last_result = INVALID_MSG;
+    return false;
+  }
+  // encoded the message and send
   std::string send_message = msg.tag + ":" + msg.data + "\n";
   ssize_t size_write = rio_writen(m_fd, send_message.c_str(), send_message.length());
   if (size_write != (ssize_t)send_message.length()){
@@ -79,15 +87,17 @@ bool Connection::send(const Message &msg) {
 }
 
 bool Connection::receive(Message &msg) {
-  // TODO: receive a message, storing its tag and data in msg
+  // receive a message, storing its tag and data in msg
   // return true if successful, false if not
-  // make sure that m_last_result is set appropriatel
+  // set m_last_result is appropriatel
   char receive_message[Message::MAX_LEN + 1];
+  //read the message
   ssize_t size_read = rio_readlineb(&m_fdbuf, receive_message, Message::MAX_LEN);
   if (size_read <= 0){
     m_last_result = EOF_OR_ERROR;
     return false;
   }
+  // delete the encoded new line character in the message
   if (receive_message[size_read - 1] == '\n'){
     receive_message[size_read - 1] = '\0';
   }else{
@@ -96,6 +106,7 @@ bool Connection::receive(Message &msg) {
   std::string tag;
   std::string data;
   std::string all_message(receive_message);
+  //split the encoded message and store the tag and data
   size_t split_index = all_message.find(':');
   if (split_index == std::string::npos){
     m_last_result = EOF_OR_ERROR;
--- csf_assign05/receiver.cpp
+++ csf_assign05/receiver.cpp
@@ -20,54 +20,79 @@ int main(int argc, char **argv) {
 
   Connection conn;
 
-  // TODO: connect to server
+  // connect to the server
   conn.connect(server_hostname, server_port);
   if (!conn.is_open()){
     std::cerr << "Could not connect to the server" << std::endl;
     return 1;
   }
-  // TODO: send rlogin and join messages (expect a response from
-  //       the server for each one)
+  // send rlogin and join messages
   Message msg_login = Message(TAG_RLOGIN, username); 
-  conn.send(msg_login);
+  if(!conn.send(msg_login)){
+    std::cerr << "cannot send message" << std::endl;
+    return 1;
+  }
   Message receive_login;
-  conn.receive(receive_login);
+  if(!conn.receive(receive_login)){
+    std::cerr << "cannot receive message" << std::endl;
+    return 1;
+  }
   if (receive_login.tag == TAG_ERR){
     std::cerr << receive_login.data << std::endl;
     return 1;
   }
   if (receive_login.tag != TAG_OK){
-    //std::cerr << "unexpected message received" << std::endl;
+    std::cerr << "unexpected message received" << std::endl;
     return 1;
   }
   //receive_login.tag == TAG_OK
   Message msg_join = Message(TAG_JOIN, room_name);
-  conn.send(msg_join);
+  if(!conn.send(msg_join)){
+    std::cerr << "cannot send message" << std::endl;
+    return 1;
+  }
   Message receive_join;
-  conn.receive(receive_join);
+  if(!conn.receive(receive_join)){
+    std::cerr << "cannot receive message" << std::endl;
+  }
   if(receive_join.tag == TAG_ERR){
     std::cerr << receive_join.data << std::endl;
     return 1;
   }
   if(receive_join.tag != TAG_OK){
-    //std::cerr << "unexpected message received" << std::endl;
+    std::cerr << "unexpected message received" << std::endl;
     return 1;    
   }
-  
-  // TODO: loop waiting for messages from server
-  //       (which should be tagged with TAG_DELIVERY)
+  //receive_join.tag == TAG_OK, can receive message delivered now
+  //waiting for messages from server
   while(1){
+    // return if server is closed
+    if (!conn.is_open()){
+      std::cerr << "could not connect to the server" << std::endl;
+      return 1;
+    }
     Message msg_deliver;
     conn.receive(msg_deliver);
     if (msg_deliver.tag == TAG_ERR){
       std::cerr << msg_deliver.data << std::endl;
     }else if (msg_deliver.tag != TAG_DELIVERY){
-      //std::cerr << "unexpected message received" << std::endl;
+      continue;
     } else{
+      //split the delivered message
       std::string delivery = msg_deliver.data;
       size_t split_index = delivery.find(":");
       std::string output = delivery.substr(split_index + 1);
-      std::cout << output << std::endl;
+      std::string room_check = delivery.substr(0, split_index);
+      //check if the message is sent to the current room
+      if (room_check == room_name){
+	size_t split_index2 = output.find(":");
+	std::string sender_name = output.substr(0, split_index2);
+	std::string message_send = output.substr(split_index2 + 1);
+	//print message in desired format
+        std::cout << sender_name + ": " + message_send << std::endl;
+      }else{
+        continue;
+      }
     }
   }
 
