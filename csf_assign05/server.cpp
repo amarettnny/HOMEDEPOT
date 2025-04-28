f9bf98d0beb09880544a96e8b65793298920a793
Tianji Li <tianjili66@gmail.com>
2025-04-28 18:28:51 -0400

    initialization of server

--- csf_assign05/server.cpp
+++ csf_assign05/server.cpp
@@ -25,22 +25,92 @@
 ////////////////////////////////////////////////////////////////////////
 
 namespace {
-
+struct Client_thread{
+  Server* server;
+  Connection* conn;
+};
+//void chat_with_sender(Connection* conn, Room* room, User* user){
+//  return;
+//}
+void chat_with_receiver(Connection* conn, Room* room, User* user){
+  return;
+}
 void *worker(void *arg) {
   pthread_detach(pthread_self());
 
   // TODO: use a static cast to convert arg from a void* to
   //       whatever pointer type describes the object(s) needed
   //       to communicate with a client (sender or receiver)
+  Client_thread* client_conn = static_cast<Client_thread*>(arg);
+  Connection* conn = client_conn->conn;
+  Server* server = client_conn->server;
 
   // TODO: read login message (should be tagged either with
   //       TAG_SLOGIN or TAG_RLOGIN), send response
+  Message receive_login;
+  if (!conn->receive(receive_login)){
+    //error handling
+    delete conn;
+    return nullptr;
+  } 
+  if (receive_login.tag == TAG_SLOGIN || receive_login.tag ==  TAG_RLOGIN){
+    Message response_login;
+    response_login.tag = TAG_OK;
+    response_login.data = "logged in";
+    if (!conn->send(response_login)){
+      //error handling
+      delete conn;
+      return nullptr;
+    } 
+  } else{
+    //error handling
+    Message response_err_login;
+    response_err_login.tag = TAG_ERR;
+    response_err_login.data = "Invalid message.";
+    conn->send(response_err_login);
+    delete conn;
+    return nullptr;
+  }
+  Message receive_join;
+  if (!conn->receive(receive_join)){
+   //error handling
+   delete conn;
+   return nullptr;
+  }
+  if(receive_join.tag == TAG_JOIN){
+    Message response_join;
+    response_join.tag = TAG_OK;
+    response_join.data = "joined";
+    if (!conn->send(response_join)){
+      //error handling
+      delete conn;
+      return nullptr;
+    }
+  } else{
+    //error handling
+    Message response_err_join;
+    response_err_join.tag = TAG_ERR;
+    response_err_join.data = "Invalid message.";
+    conn->send(response_err_join);
+    delete conn;
+    return nullptr;
+  }
 
   // TODO: depending on whether the client logged in as a sender or
   //       receiver, communicate with the client (implementing
   //       separate helper functions for each of these possibilities
   //       is a good idea)
-
+  User* user = new User(receive_login.data);
+  Room* room = server->find_or_create_room(receive_join.data);
+  room->add_member(user); 
+  if (receive_login.tag == TAG_SLOGIN){
+    //chat_with_sender(conn);
+  }else{
+    chat_with_receiver(conn, room, user);
+  }
+  delete conn;
+  delete user;
+  delete room;
   return nullptr;
 }
 
@@ -76,6 +146,15 @@ bool Server::listen() {
 void Server::handle_client_requests() {
   // TODO: infinite loop calling accept or Accept, starting a new
   //       pthread for each connected client
+  while(1){
+    int client_fd = Accept(m_ssock, nullptr, nullptr);
+    Connection* conn = new Connection(client_fd);
+    pthread_t thread;
+    Client_thread* client_conn = new Client_thread();
+    client_conn->server = this;
+    client_conn->conn = conn;
+    Pthread_create(&thread, nullptr, worker, client_conn);
+  }
 }
 
 Room *Server::find_or_create_room(const std::string &room_name) {
